39448e953c53119091d2e53817a5770c03ca914f
bearjin90 <bearjin90@gmail.com>
2021-08-31 00:12:16 +0900

    Update: 컨텐츠 컴포넌트 추가로 컴포넌트 구조 변경 및 css 수정

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import './styles/scss/app.scss';
 import Header from './components/Header';
 import VisualSlider from './components/VisualSlider';
-import MainTab from './components/MainTab';
-import SubTab from './components/SubTab';
-import SubVisualSlider from './components/SubVisualSlider';
+import ProductContent from './components/ProductContent';
 
 const App = () => {
   return (
     <main id="main" className="main">
       <Header />
       <VisualSlider />
-      <MainTab />
-      <SubTab category={'ranking'} />
-      <SubVisualSlider category={'ranking'} />
+      <ProductContent />
     </main>
   );
-}
+};
 
 export default App;
