fe08e42d44e0a40d0d19c1d62a746f7eda21090a
bearjin90 <bearjin90@gmail.com>
2021-08-30 22:12:41 +0900

    Update: 메인탭 마크업 구조 및 디자인 수정

--- src/components/MainTab/index.js
+++ src/components/MainTab/index.js
@@ -4,15 +4,13 @@ import mainTabData from '../../_data/mainTabData';
 const MainTab = () => {
   return (
     <div id="mainTab" className="main-tab">
-      <ul className="main-tab__list">
-        {
-          mainTabData.map((elm, idx) => {
-            return (
-              <li key={elm + idx} className="main-tab__item"><button type="button" className="main-tab__btn">{elm}</button></li>
-            );
-          })
-        }
-      </ul>
+      {
+        mainTabData.map((elm, idx) => {
+          return (
+            <button key={elm + idx} type="button" className="main-tab__btn">{elm}</button>
+          );
+        })
+      }
     </div>
   );
 };
