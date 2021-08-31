a0faa55f2a4471c16cd8dd692b80e8029374c505
bearjin90 <bearjin90@gmail.com>
2021-08-31 17:34:11 +0900

    Add: 랭킹 컨텐츠 컴포넌트

--- src/components/RankingContent/index.js
+++ src/components/RankingContent/index.js
@@ -6,6 +6,18 @@ import RankingList from '../RankingList';
 const RankingContent = () => {
   return (
     <section className="ranking">
+      <div className="ranking-title">
+        <div className="ranking-title__left">
+          <h3 className="ranking-title__title">상품랭킹</h3>
+          <a className="ranking-title__link">전체</a>
+        </div>
+        <div className="ranking-title__right">
+          <button type="button" className="ranking-title__all-age-btn">
+            전체 연령대
+            <img src={require('../../assets/images/ico/ico_menu.png').default} className="ranking-title__menu-icon" />
+          </button>
+        </div>
+      </div>
       <SubTab category={'ranking'} />
       <RankingList start={0} end={5} />
       <SubVisualSlider category={'ranking'} />
