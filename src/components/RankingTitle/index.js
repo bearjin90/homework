b5e5b2160a8f8bdbbad05b32abbd119a80b770f7
bearjin90 <bearjin90@gmail.com>
2021-08-31 17:42:02 +0900

    Update: 랭킹 타이틀 컴포넌트로 분리 및 컨텐츠 간격 수정

---
+++ src/components/RankingTitle/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const RankingTitle = () => {
+  return (
+    <div className="ranking-title">
+      <div className="ranking-title__left">
+        <h3 className="ranking-title__title">상품랭킹</h3>
+        <a className="ranking-title__link">전체</a>
+      </div>
+      <div className="ranking-title__right">
+        <button type="button" className="ranking-title__all-age-btn">
+          전체 연령대
+          <img src={require('../../assets/images/ico/ico_menu.png').default} className="ranking-title__menu-icon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default RankingTitle;
