c330d1a901088c226c18b6f55c768531fb556db7
bearjin90 <bearjin90@gmail.com>
2021-09-01 19:04:01 +0900

    Update: 업데이트 프로덕트 리스트 추가

--- src/components/product/update/UpdateContent/index.js
+++ src/components/product/update/UpdateContent/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import SubTab from '../../../SubTab';
+import SubVisualSlider from '../../../SubVisualSlider';
 import ButtonWrap from '../../../buttons/ButtonWrap';
 import RefreshButton from '../../../buttons/RefreshButton';
 import UpdateNewsList from '../UpdateNewsList';
 import UpdateTitle from '../UpdateTitle';
+import UpdateProductContent from '../UpdateProductCotent';
 
 const UpdateContent = () => {
   return (
@@ -12,6 +15,11 @@ const UpdateContent = () => {
         <RefreshButton text={'새로운  추천'} />
       </ButtonWrap>
       <UpdateTitle />
+      <SubTab category={'update'} />
+      <UpdateProductContent category={'product1'} />
+      <UpdateProductContent category={'product2'} />
+      <SubVisualSlider category={'ranking'} />
+      <UpdateProductContent category={'product3'} />
     </section>
   );
 };
