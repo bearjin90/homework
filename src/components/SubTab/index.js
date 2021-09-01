30f1ac5c83f84099a7f9dd99a334b4c49868e35f
bearjin90 <bearjin90@gmail.com>
2021-09-01 23:31:05 +0900

    Update: 서브탭 활성화 및 스크롤 이동 기능 추가

--- src/components/SubTab/index.js
+++ src/components/SubTab/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import subTabData from '../../_data/subTabData';
 
 const SubTab = ({ category }) => {
+  const onClickTab = useCallback((e) => {
+    const target = e.target;
+    const targetParent = e.target.parentNode;
+    targetParent.querySelector('.sub-tab__btn--active').classList.remove('sub-tab__btn--active');
+    target.classList.add('sub-tab__btn--active');
+    target.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
+  }, []);
+
   return (
     <div id="subTab" className="sub-tab">
       {
         subTabData[category].map((elm, idx) => {
           return (
-            <button key={elm + idx} type="button" className="sub-tab__btn">{elm}</button>
+            <button key={elm + idx} type="button" className={`sub-tab__btn ${idx === 0 ? "sub-tab__btn--active" : ''}`} onClick={onClickTab}>{elm}</button>
           );
         })
       }
-    </div>
+    </div >
   );
 };
 
