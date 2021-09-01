b2c0d48824580baeed6daa8b516426ea3a0911e8
bearjin90 <bearjin90@gmail.com>
2021-09-01 17:03:07 +0900

    Add: 업데이트 타이틀 컴포넌트

--- src/components/product/update/UpdateContent/index.js
+++ src/components/product/update/UpdateContent/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ButtonWrap from '../../../buttons/ButtonWrap';
 import RefreshButton from '../../../buttons/RefreshButton';
 import UpdateNewsList from '../UpdateNewsList';
+import UpdateTitle from '../UpdateTitle';
 
 const UpdateContent = () => {
   return (
@@ -10,6 +11,7 @@ const UpdateContent = () => {
       <ButtonWrap>
         <RefreshButton text={'새로운  추천'} />
       </ButtonWrap>
+      <UpdateTitle />
     </section>
   );
 };
---
+++ src/components/product/update/UpdateTitle/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UpdateTitle = () => {
+  return (
+    <div className="update-title">
+      <h3 className="update-title__title">업데이트 소식</h3>
+      <a href="/" className="update-title__link">전체</a>
+    </div>
+  );
+};
+
+export default UpdateTitle;
